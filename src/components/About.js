fb6731aa01a93e696374d580a14b4f062acc38c4
Anusha Balusu <anusha.balusu08@gmail.com>
2024-04-16 15:04:01 -0400

    ep-08 p-05 order of lifecycle calls for class components

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,31 @@
 import User from "./User";
 import UserClass from "./UserClass";
+import { Component } from "react";
 
-const About = () => {
+class About extends Component {
 
-    return (
-        <div>
-            <h1>About</h1>
-            <h2>This is food ordering app</h2>
-            <UserClass name={"Anusha Balusu"} loc={"Toronto"}/>
-        </div>
-    )
+    constructor(props) {
+        super(props);
+        console.log("Parent constructor called");
+    }
+
+    componentDidMount() {
+        console.log("Parent componentDidMount called");
+    }
+
+    render() {
+        console.log("Parent render called");
+        return (
+            <div>
+                <h1>About</h1>
+                <h2>This is food ordering app</h2>
+                <UserClass name={"First"} loc={"Toronto"}/>
+                <UserClass name={"Second"} loc={"US"}/>
+                <UserClass name={"Third"} loc={"India"}/>
+            </div>
+        )
+    }
+    
 };
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -8,12 +8,18 @@ class UserClass extends React.Component {
         this.state = {
             count: 0,
         }
+        console.log(props.name + " child constructor called");
+    }
+
+    componentDidMount() {
+        console.log(this.props.name +  " child componentDidMount called");
     }
 
     render() {
         const {name, loc} = this.props;
         const {count} = this.state;
 
+        console.log(this.props.name + " child render called");
         return (
             <div className="user-card">
                 <h1>Count: {count}</h1>
